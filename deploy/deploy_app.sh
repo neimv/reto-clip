97e014a4bd9c07e7a57b6ef9f0edef2616059ba9
neivm <neimv.zatara@gmail.com>
2021-09-15 11:36:49 -0500

    Squash and create final v1

--- deploy/deploy_app.sh
+++ deploy/deploy_app.sh
@@ -2,6 +2,18 @@
 
 EC2_CF=reto-ec2
 RDS_CF=reto-rds
+PORT=80
+
+Help() {
+   echo "Script to deploy app or only update server"
+   echo
+   echo "Syntax: scriptTemplate [-d|u|h]"
+   echo "options:"
+   echo "d     Deploy app"
+   echo "u     Update server"
+   echo "h     Print this Help"
+   echo
+}
 
 echo "Get the dns to connect"
 output_ec2=$(aws cloudformation describe-stacks \
@@ -45,18 +57,81 @@ echo $username
 password=$(echo $secrets | jq '.password' | tr -d '"')
 echo $password
 
-# echo "Connect and update, upgrade and install deps server EC2"
-# ssh -i reto.pem ubuntu@$output_ec2 'sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install docker.io git mysql-client-core-8.0 -y'
+chmod 600 reto.pem
 
-# echo "Set permissions to docker"
-# ssh -i reto.pem ubuntu@$output_ec2 'sudo usermod -aG docker ${USER}'
+# Operations
+function update_server() {
+    ssh -i reto.pem -o "StrictHostKeyChecking no" ubuntu@$output_ec2 'sudo apt-get update && sudo apt-get upgrade -y'
+}
 
-# echo "git clone"
-# ssh -i reto.pem ubuntu@$output_ec2 'git clone https://github.com/neimv/reto-clip.git && cd reto-clip && git checkout v1'
+function setup_init() {
+    echo "Connect and update, upgrade and install deps server EC2"
+    ssh -i reto.pem -o "StrictHostKeyChecking no" ubuntu@$output_ec2 'sudo apt-get update && sudo apt-get install docker.io git mysql-client-core-8.0 -y'
 
-echo "set credentials"
-SQL_CONNECT="$username:$password@tcp($output_rds:$output_db_port)/$output_db_name"
-echo $SQL_CONNECT
-ssh -i reto.pem ubuntu@$output_ec2 'export SQLCONNECT='"'$SQL_CONNECT'"'; echo SQLCONNECT=$SQLCONNECT > .envs'
+    echo "Set permissions to docker"
+    ssh -i reto.pem ubuntu@$output_ec2 'sudo usermod -aG docker ${USER}'
+}
 
-echo "runing docker"
+function clone_and_pull() {
+    echo "git clone"
+    ssh -i reto.pem -o "StrictHostKeyChecking no" ubuntu@$output_ec2 'git clone https://github.com/neimv/reto-clip.git && cd reto-clip && git checkout v1 && git pull origin v1'
+}
+
+function create_credentials() {
+    echo "set credentials"
+    SQL_CONNECT="$username:$password@tcp($output_rds:$output_db_port)/$output_db_name"
+    echo $SQL_CONNECT
+    ssh -i reto.pem -o "StrictHostKeyChecking no" ubuntu@$output_ec2 'printf "SQLCONNECT='"$SQL_CONNECT"'\nPORT='"$PORT"'" > .envs'
+    ssh -i reto.pem -o "StrictHostKeyChecking no" ubuntu@$output_ec2 'printf "[mysql]\nuser='"$username"'\npassword='"$password"'" > .my.cnf; chmod 0600 .my.cnf'
+}
+
+function create_table() {
+    echo "running sql"
+    ssh -i reto.pem -o "StrictHostKeyChecking no" ubuntu@$output_ec2 'cd reto-clip/services; mysql -u '"$username"' -h '"$output_rds"' -P '"$output_db_port"' '"$output_db_name"' < models.sql'
+}
+
+function insert_data_test() {
+    ssh -i reto.pem -o "StrictHostKeyChecking no" ubuntu@$output_ec2 'cd reto-clip/services; mysql -u '"$username"' -h '"$output_rds"' -P '"$output_db_port"' '"$output_db_name"' < models.inserts.sql'
+}
+
+# echo "running docker"
+function run_docker() {
+    ssh -i reto.pem -o "StrictHostKeyChecking no" ubuntu@$output_ec2 'cd reto-clip/services; docker build -t go-ws-pet . && docker run -d -p '"$PORT"':'"$PORT"' --env-file=/home/ubuntu/.envs go-ws-pet'
+}
+
+function Deploy() {
+    echo "In deploy"
+    update_server
+    setup_init
+    clone_and_pull
+    create_credentials
+    create_table
+    insert_data_test
+    run_docker
+}
+
+function Update() {
+    echo "In update"
+    update_server
+}
+
+while getopts ":hud" option; do
+    echo "test"
+    case $option in
+        h) # display Help
+            Help
+            exit;;
+
+        d) # deploy
+            Deploy
+            exit;;
+
+        u) # update
+            Update
+            exit;;
+
+        \?) # Invalid option
+            echo "Error: Invalid option"
+            exit;;
+    esac
+done
--- services/models.sql
+++ services/models.sql
@@ -3,8 +3,9 @@ CREATE DATABASE IF NOT EXISTS reto;
 USE reto;
 
 CREATE TABLE IF NOT EXISTS pet (
-    name    VARCHAR(20) NULL,
+    name    VARCHAR(20) NOT NULL,
     owner   VARCHAR(20) NULL,
     species VARCHAR(20) NULL,
-    sex     CHAR(1) NULL
+    sex     CHAR(1) NULL,
+    PRIMARY KEY (name)
 );
